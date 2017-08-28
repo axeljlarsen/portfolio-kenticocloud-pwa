6bf55531177f92a3b3a1921488090f056776ecf3
SKYLINE\alarsen <alarsen@skylinetechnologies.com>
2017-08-27 21:12:55 -0500

    added data storage

--- pwa-experiment/src/Stores/PortfolioItem.js
+++ pwa-experiment/src/Stores/PortfolioItem.js
@@ -1,4 +1,6 @@
 import Client from "../Client.js";
+import { reactLocalStorage } from 'reactjs-localstorage';
+import localForage from "localforage";
 
 let portfolioItemList = [];
 let portfolioItemListCapacity = 0;
@@ -9,69 +11,118 @@ let portfolioItemDetailsPromises = {};
 let changeListeners = [];
 
 let notifyChange = () => {
-  changeListeners.forEach((listener) => {
-    listener();
-  });
+    changeListeners.forEach((listener) => {
+        listener();
+    });
+
+}
+
+let storageKey = {
+    portfolioItems: 'portfolioItems',
+    foragePortfolioItems: 'foragePortfolioItems'
+}
+
+let updatePortfolioItemsOfflineStore = () => {
+    var listToStore = [];
+    for (var i = 0; i < portfolioItemList.length; i++) {
+        var key = portfolioItemList[i].elements.friendly_url.value;
+        listToStore[key] = portfolioItemList[i];
+    }
+    //reactLocalStorage.setObject(storageKey.portfolioItems, listToStore);
+    localForage.setItem(storageKey.foragePortfolioItems, listToStore).then(function (value) {
+        console.log('set', value);
+    }).catch(function (err) {
+        console.log(err);
+    });
 }
 
 class PortfolioItemStore {
 
-  // Actions
+    // Actions
 
-  providePortfolioItem(portfolioItemSlug) {
-    if (portfolioItemDetailsPromises[portfolioItemSlug]) {
-      return;
+    providePortfolioItem(portfolioItemSlug) {
+        //var storedList = reactLocalStorage.getObject(storageKey.portfolioItems);
+        var storedList = localForage.getItem(storageKey.foragePortfolioItems).then(function (value) {
+            if (value != null && value[portfolioItemSlug]) {
+                console.log('get', value);
+                portfolioItemDetails[portfolioItemSlug] = value[portfolioItemSlug];
+                return value[portfolioItemSlug];
+            }
+            else {
+                if (portfolioItemDetailsPromises[portfolioItemSlug]) {
+                    return;
+                }
+
+                portfolioItemDetailsPromises[portfolioItemSlug] = Client.getItems({
+                    "system.type": "portfolio_item",
+                    "elements.friendly_url": portfolioItemSlug
+                }).then((response) => {
+                    if (response.items.length > 0) {
+                        portfolioItemDetails[portfolioItemSlug] = response.items[0];
+                        notifyChange();
+                        updatePortfolioItemsOfflineStore();
+                    }
+                });
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+
     }
 
-    portfolioItemDetailsPromises[portfolioItemSlug] = Client.getItems({
-      "system.type": "portfolio_item",
-      "elements.friendly_url": portfolioItemSlug
-    }).then((response) => {
-      if (response.items.length > 0) {
-        portfolioItemDetails[portfolioItemSlug] = response.items[0];
-        notifyChange();
-      }
-    });
-  }
+    providePortfolioItems(count) {
+        //var storedList = reactLocalStorage.getObject(storageKey.portfolioItems);
+        var storedList = localForage.getItem(storageKey.foragePortfolioItems).then(function (value) {
+            if (value != null ) {
+                console.log('get', value);
+                portfolioItemList = value;
+                return value;
+            }
+            else {
+                if (count <= portfolioItemListCapacity) {
+                    return;
+                }
 
-  providePortfolioItems(count) {
-    if (count <= portfolioItemListCapacity) {
-      return;
+                portfolioItemListCapacity = count;
+
+                Client.getItems({
+                    "system.type": "portfolio_item",
+                    "elements": "title,thumbnail_image,post_date,description,friendly_url",
+                    "order": "elements.post_date[DESC]"
+                }).then((response) => {
+                    portfolioItemList = response.items;
+                    notifyChange();
+                    updatePortfolioItemsOfflineStore();
+                });
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+
     }
 
-    portfolioItemListCapacity = count;
+    // Methods
 
-    Client.getItems({
-      "system.type": "portfolio_item",
-      "elements": "title,thumbnail_image,post_date,description,friendly_url",
-      "order": "elements.post_date[DESC]"
-    }).then((response) => {
-      portfolioItemList = response.items;
-      notifyChange();
-    });
-  }
+    getPortfolioItem(portfolioItemSlug) {
+        return portfolioItemDetails[portfolioItemSlug];
+    }
 
-  // Methods
+    getPortfolioItems(count) {
+        return portfolioItemList.slice(0, count);
+    }
 
-  getPortfolioItem(portfolioItemSlug) {
-    return portfolioItemDetails[portfolioItemSlug];
-  }
+    // Listeners
 
-  getPortfolioItems(count) {
-    return portfolioItemList.slice(0, count);
-  }
+    addChangeListener(listener) {
+        changeListeners.push(listener);
+    }
 
-  // Listeners
-
-  addChangeListener(listener) {
-    changeListeners.push(listener);
-  }
-
-  removeChangeListener(listener) {
-    changeListeners = changeListeners.filter((element) => {
-      return element !== listener;
-    });
-  }
+    removeChangeListener(listener) {
+        changeListeners = changeListeners.filter((element) => {
+            return element !== listener;
+        });
+    }
 
 }
 

0a4c3d036111681be64cb97cc374d82fe9e47f64
SKYLINE\alarsen <alarsen@skylinetechnologies.com>
2018-05-09 10:48:31 -0500

    fix back button issue on ios fix rendering in ios

--- skyline-portfolio-pwa/src/Pages/Portfolio.js
+++ skyline-portfolio-pwa/src/Pages/Portfolio.js
@@ -78,37 +78,35 @@ class Portfolio extends Component {
       result.push(
         <div className={bootstrapVersion == 4 ? 'col' : 'col-xs-12' + ' col-sm-6 col-md-4 col-lg-3'} key={counter++}>
 
-          <div className="card card-portfolio-item">
-            <Link to={link}>
-              <div className="portfolioItem-tile-image-wrapper">
+          <Link className="card card-portfolio-item" to={link}>
+            <div className="portfolioItem-tile-image-wrapper">
               <img alt={'PortfolioItem ' + title} className="portfolioItem-tile-image" src={imageUrl} title={'PortfolioItem ' + title} />
 
-              </div>
-              <div className="card-body">
-                <h2 className="h4">
-                  {title}
-                </h2>
-                <RichTextElement className={'portfolioItem-detail-content ' + ((description.value.length <= 3) ? 'd-none' : '')} element={description} />
+            </div>
+            <div className="card-body">
+              <h2 className="h4">
+                {title}
+              </h2>
+              <RichTextElement className={'portfolioItem-detail-content ' + ((description.value.length <= 3) ? 'd-none' : '')} element={description} />
 
-              </div>
-              <div className="card-footer portfolioItem-tile-features">
-                {
-                  portfolioItem.features.map((feature, index) => {
-                    if (feature) {
-                      let featureImageUrl = (feature.icon.value.length) ? feature.icon.value[0].url : '';
-                      let featureImageDesc = (feature.icon.value.length) ? feature.icon.value[0].description : '';
-                      return (
-                        <div className="align-middle text-center" key={index}>
-                          <img alt={featureImageDesc} className="img-responsive icon mr-2" src={featureImageUrl} title={featureImageDesc} />
-                          <span>{feature.caption.value}</span>
-                        </div>
-                      )
-                    }
-                  })
-                }
-              </div>
-            </Link>
-          </div>
+            </div>
+            <div className="card-footer portfolioItem-tile-features">
+              {
+                portfolioItem.features.map((feature, index) => {
+                  if (feature) {
+                    let featureImageUrl = (feature.icon.value.length) ? feature.icon.value[0].url : '';
+                    let featureImageDesc = (feature.icon.value.length) ? feature.icon.value[0].description : '';
+                    return (
+                      <div className="align-middle text-center" key={index}>
+                        <img alt={featureImageDesc} className="img-responsive icon mr-2" src={featureImageUrl} title={featureImageDesc} />
+                        <span>{feature.caption.value}</span>
+                      </div>
+                    )
+                  }
+                })
+              }
+            </div>
+          </Link>
         </div>
       );
 
